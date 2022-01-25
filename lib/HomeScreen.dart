0214e9c1e640f935e1bd0ddfd676f0a233dc1e14
Ali Cagatay <aliccagatay@gmail.com>
2022-01-25 14:58:30 +0000

    Created the initial bone of the general user interface.

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:nameless_secret_project/ProfilePage.dart';
+import 'package:nameless_secret_project/MainPage.dart';
+import 'package:nameless_secret_project/SearchPage.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    ProfilePage(),
+    MainPage(),
+    SearchPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 30,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
