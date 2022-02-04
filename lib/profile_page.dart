e0eeaa9b341c4c9a8dd00c7b1abffc90f95e0f5a
Ali Cagatay <aliccagatay@gmail.com>
2022-02-04 13:13:40 +0000

    Progile page UI update

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -9,90 +9,111 @@ class ProfilePage extends StatelessWidget {
       home: Scaffold(
         body: ListView(
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  const CircleAvatar(
-                    backgroundColor: Colors.teal,
-                    radius: 80,
-                    child: Center(
-                      child: Text(
-                        "U",
-                        style: TextStyle(
-                          fontSize: 120,
-                          color: Colors.white,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:  [
+                    Container(
+                      padding: const EdgeInsets.only(right: 20, left: 20),
+                      child: const CircleAvatar(
+                        backgroundColor: Colors.teal,
+                        radius: 80,
+                        child: Text(
+                          "U",
+                          style: TextStyle(
+                            fontSize: 120,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
+                    const Expanded(
+                      child: Text(
+                        "Username",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  Container(
-                    padding: const EdgeInsets.all(40),
-                    child: const Text(
-                      "Username",
+                  margin: const EdgeInsets.all(30),
+                  height: 80,
+                  child: const Center(
+                    child: Text(
+                      "My Wallet",
                       style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 30,
+                        fontSize: 25,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    margin: const EdgeInsets.all(30),
-                    height: 80,
-                    child: const Center(
-                      child: Text(
-                        "My Wallet",
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: Colors.white,
-                        ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(30),
+                  height: 80,
+                  child: const Center(
+                    child: Text(
+                      "My Stuff",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    margin: const EdgeInsets.all(30),
-                    height: 80,
-                    child: const Center(
-                      child: Text(
-                        "My Stuff",
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: Colors.white,
-                        ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(30),
+                  height: 80,
+                  child: const Center(
+                    child: Text(
+                      "Wishlist",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    margin: const EdgeInsets.all(30),
-                    height: 80,
-                    child: const Center(
-                      child: Text(
-                        "Messages",
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: Colors.white,
-                        ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(30),
+                  height: 80,
+                  child: const Center(
+                    child: Text(
+                      "Messages",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
